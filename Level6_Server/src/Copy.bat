8930f119881ffc21147bb28d4d5464fa4acc5d71
baize-live <1921676794@qq.com>
2023-05-29 16:10:36 +0800

    添加一些测试和日志

--- Level6_Server/src/Copy.bat
+++
@@ -1,1 +0,0 @@
-xcopy "..\build\libs\Level6_Server-0.1.jar" ".\" /y
---
+++ Level6_Server/src/test/java/live/baize/Level6ServerTest.java
@@ -0,0 +1,18 @@
+package live.baize;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Level6ServerTest {
+
+    @Test
+    void test() {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        System.out.println(format.format(new Date("2023-05-01 00:00:00")));
+//        System.out.println(format.format(new Date()));
+//        System.out.println(format.format(new Date()));
+    }
+
+}
