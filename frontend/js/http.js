28531b5334dbb5d6ff4a71a353072b7fce4d3356
baize-live <1921676794@qq.com>
2023-06-03 19:52:46 +0800

    实现前端登录功能

---
+++ frontend/js/http.js
@@ -0,0 +1,134 @@
+/*
+* 传参：object
+* obj.path: 请求路径
+* obj.method: 请求方法 默认get
+* obj.data: 请求参数 默认为空
+* */
+function requests(obj) {
+    let method;
+    if (obj.method) {
+        obj.method = obj.method.toUpperCase();
+        method = obj.method;
+    } else {
+        method = "GET";
+    }
+
+    const urlencoded = new URLSearchParams();
+    // get请求的data参数拼接到url后面，post等其他请求的data参数放到body
+    if (obj.method === "GET") {
+        if (obj.data) {
+            // URL编码
+            let ec = encodeURIComponent;
+            // 获取data键名
+            let queryParams = Object.keys(obj.data).map(k => `${ec(k)}=${ec(obj.data[k])}`).join('&');
+            if (queryParams) obj.path += `?${queryParams}`;
+            console.log(obj.path)
+        }
+    } else {
+        for (const key in obj.data) {
+            urlencoded.append(key, obj.data[key]);
+        }
+    }
+
+    const headers = new Headers();
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+    const requestOptions = {
+        method: method,
+        headers: headers,
+        redirect: 'follow'
+    };
+
+    if (method !== "GET" && method !== "HEAD") {
+        requestOptions.body = urlencoded
+    }
+
+    console.log(requestOptions);
+    // 发送请求并返回 promise 对象，注意 fetch不会拦截400/500等异常请求️，只有网络不通时才会失败
+    return fetch(baseUrl + obj.path, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+            return new Promise(function (resolve, reject) {
+                const global_code = parseInt(data.code.toString().substring(0, 3));
+                if (global_code !== 400 && global_code !== 500) {
+                    //响应码不是400，500，返回数据
+                    resolve(data)
+                } else {
+                    reject("请求失败：" + data.code.toString() + data.msg);
+                }
+            })
+        })
+        .catch((error) => {
+            console.log('error', error);
+        })
+}
+
+function student_requests(obj) {
+    obj.path = "/student" + obj.path
+
+    // 发送请求并返回 promise 对象，注意 fetch不会拦截400/500等异常请求️，只有网络不通时才会失败
+    return requests(obj)
+        .then((data) => {
+            return new Promise(function (resolve, reject) {
+                // TODO: 登录判断
+                if (data.code === Student_Not_Login.code) {
+                    // 重定向
+                    setTimeout(() => {
+                        location.href = "../login-register.html";
+                    }, 1000)
+                } else {
+                    resolve(data)
+                }
+            })
+        })
+        .catch((error) => {
+            console.log('error', error);
+        });
+}
+
+function admin_requests(obj) {
+    obj.path = "/admin" + obj.path
+
+    // 发送请求并返回 promise 对象，注意 fetch不会拦截400/500等异常请求️，只有网络不通时才会失败
+    return requests(obj)
+        .then((data) => {
+            return new Promise(function (resolve, reject) {
+                // TODO: 登录判断
+                if (data.code === Admin_Not_Login.code) {
+                    // 重定向
+                    setTimeout(() => {
+                        location.href = "../login-register.html";
+                    }, 1000)
+                } else {
+                    resolve(data)
+                }
+            })
+        })
+        .catch((error) => {
+            console.log('error', error);
+        });
+}
+
+function teacher_requests(obj) {
+    obj.path = "/teacher" + obj.path
+
+    // 发送请求并返回 promise 对象，注意 fetch不会拦截400/500等异常请求️，只有网络不通时才会失败
+    return requests(obj)
+        .then((data) => {
+            return new Promise(function (resolve, reject) {
+                // TODO: 登录判断
+                if (data.code === Teacher_Not_Login.code) {
+                    // 重定向
+                    setTimeout(() => {
+                        location.href = "../login-register.html";
+                    }, 1000)
+                } else {
+                    resolve(data)
+                }
+            })
+        })
+        .catch((error) => {
+            console.log('error', error);
+        });
+}
+
---
+++ frontend/js/login.js
@@ -0,0 +1,100 @@
+// 获取表单元素
+const Admin_Login = document.getElementById("Admin-Login");
+const Admin_Email = document.getElementById("Admin-E-mail");
+const Admin_Passwd = document.getElementById("Admin-Passwd");
+
+const Student_Login = document.getElementById("Student-Login");
+const Student_IDCard = document.getElementById("Student-IDCard");
+const Student_Passwd = document.getElementById("Student-Passwd");
+
+const Teacher_Login = document.getElementById("Teacher-Login");
+const Teacher_Email = document.getElementById("Teacher-E-mail");
+const Teacher_Passwd = document.getElementById("Teacher-Passwd");
+
+
+// 学生登录
+Student_Login.addEventListener("submit", (event) => {
+    // 阻止默认行为，如刷新页面
+    event.preventDefault();
+
+    let object = {
+        path: "/login",
+        method: "GET",
+        data: {
+            idCard: Student_IDCard.value,
+            password: Student_Passwd.value,
+        }
+    }
+
+    student_requests(object)
+        .then((data) => {
+            if (data.code === Student_Login_Success.code) {
+                alert(data.msg);
+                // 跳转到另一个界面
+                setTimeout(() => {
+                    location.href = "./index.html";
+                }, 1000)
+            } else if (data.code === Student_Login_Failure.code) {
+                alert(data.msg);
+            }
+        });
+
+});
+
+// 管理员登录
+Admin_Login.addEventListener("submit", (event) => {
+    // 阻止默认行为，如刷新页面
+    event.preventDefault();
+
+    let object = {
+        path: "/login",
+        method: "GET",
+        data: {
+            email: Admin_Email.value,
+            password: Admin_Passwd.value,
+        }
+    }
+
+    admin_requests(object)
+        .then((data) => {
+            if (data.code === Admin_Login_Success.code) {
+                alert(data.msg);
+                // 跳转到另一个界面
+                setTimeout(() => {
+                    location.href = "./admin-pages/admin-index.html";
+                }, 1000)
+            } else if (data.code === Admin_Login_Failure.code) {
+                alert(data.msg);
+            }
+        });
+
+});
+
+// 老师登录
+Teacher_Login.addEventListener("submit", (event) => {
+    // 阻止默认行为，如刷新页面
+    event.preventDefault();
+
+    let object = {
+        path: "/login",
+        method: "GET",
+        data: {
+            email: Teacher_Email.value,
+            password: Teacher_Passwd.value,
+        }
+    }
+
+    teacher_requests(object)
+        .then((data) => {
+            if (data.code === Teacher_Login_Success.code) {
+                alert(data.msg);
+                // 跳转到另一个界面
+                setTimeout(() => {
+                    location.href = "./teacher-pages/teacher-index.html";
+                }, 1000)
+            } else if (data.code === Teacher_Login_Failure.code) {
+                alert(data.msg);
+            }
+        });
+
+});
