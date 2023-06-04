2028f11a5487f1168561e588afffaef0258fa5a4
baize-live <1921676794@qq.com>
2023-06-04 20:16:09 +0800

    打分功能实现

--- frontend/js/teacherScoring.js
+++ frontend/js/teacherScoring.js
@@ -1,3 +1,33 @@
+let examID = document.getElementById("examID");
+let writing = document.getElementById("essay-answer");
+let translate = document.getElementById("translate-answer");
+let registrationId = 0;
+
+function getPaperInfo_fun() {
+    let getPaperInfo = {
+        path: "/getPaperInfo",
+        method: "GET",
+        data: {
+            examId: examID.value === "" ? 1 : examID.value
+        }
+    }
+    teacher_requests(getPaperInfo)
+        .then((data) => {
+            if (data.code === Get_Paper_Success.code) {
+                registrationId = data.data.registrationId
+                writing.innerText = data.data.writing;
+                translate.innerText = data.data.translation;
+            } else if (data.code === Without_Paper.code) {
+                registrationId = 0;
+                writing.innerText = "";
+                translate.innerText = "";
+                swal(data.msg, data.msg, 'success');
+            } else {
+                swal(data.msg, data.msg, 'error');
+            }
+        });
+}
+
 window.onload = function () {
     let isLogin = {
         path: "/isLogin",
@@ -35,29 +65,44 @@ window.onload = function () {
             });
         }
     });
+
+    getPaperInfo_fun();
 }
 
-//
+let Submit_Score = document.getElementById("Submit-Score");
+Submit_Score.addEventListener("click", (event) => {
+    // 阻止默认行为，如刷新页面
+    event.preventDefault();
 
-//
-// const myHeaders = new Headers();
-// myHeaders.append("User-Agent", "Apifox/1.0.0 (https://apifox.com)");
-//
-// let getPaperAnswer = {
-//     method: 'GET',
-//     headers: myHeaders,
-//     redirect: 'follow'
-// };
-// let examID = document.getElementById("PaperID");
-// const essay = document.getElementById("essay-answer");
-// const essay_answer = '作文题试卷情况';
-//
-// essay.innerHTML = essay_answer;
-//
-//
-// fetch("https://www.baize.live/cet6/teacher/getPaperInfo?examId=<examId>", getPaperAnswer)
-//     .then(response => response.text())
-//     .then(result => console.log(result))
-//     .catch(error => console.log('error', error));
-//
-//
+    let Translate_Score = document.getElementById("Translate-Score");
+    let Essay_Score = document.getElementById("Essay-Score");
+
+    // 数据
+    let scoring = {
+        path: "/scoring",
+        method: "GET",
+        data: {
+            registrationId: registrationId,
+            scores: parseInt(Translate_Score.value) + parseInt(Essay_Score.value)
+        }
+    }
+
+    teacher_requests(scoring).then((data) => {
+        if (data.code === Scoring_Success.code) {
+            swal(data.msg, data.msg, 'success');
+            Essay_Score.value = 0;
+            Translate_Score.value = 0;
+            getPaperInfo_fun();
+        } else {
+            swal('错误', '错误', 'error');
+        }
+    });
+});
+
+let selectExam = document.getElementById("select-Exam");
+selectExam.addEventListener("click", (event) => {
+    // 阻止默认行为，如刷新页面
+    event.preventDefault();
+
+    getPaperInfo_fun();
+});
